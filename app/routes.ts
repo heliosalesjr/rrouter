3861b73807d074cef14f348680f78eb4ae4c155b
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2025-01-22 17:26:08 -0300

    cleanup

--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,4 @@
 import { type RouteConfig, index } from "@react-router/dev/routes";
+//index is a function, RouteConfig is a type
 
 export default [index("routes/home.tsx")] satisfies RouteConfig;
--- app/welcome/welcome.tsx
+++
@@ -1,9 +0,0 @@
-
-export function Welcome() {
-  return (
-    <>
-    <h1>Hello Router</h1>
-    <p>Here we go</p>
-    </>
-  );
-};
