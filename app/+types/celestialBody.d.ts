d92641955f69f25301e4c03cc651236a5cb336f9
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2025-01-28 18:34:38 -0300

    packages update

--- app/+types/celestialBody.d.ts
+++
@@ -1,8 +0,0 @@
-import type { LoaderFunctionArgs } from "react-router";
-
-export namespace Route {
-  export type LoaderArgs = LoaderFunctionArgs;
-  export type ComponentProps = {
-    loaderData: any; // Ajuste o tipo conforme necessário para os dados do corpo celeste
-  };
-}
